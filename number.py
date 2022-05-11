3e1508ffa6cd10689e0e6516cc052dfc89c7e721
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2022-05-11 12:00:47 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,9 +1,1 @@
-print('Sei din scale er kotha kno extra
-pakamo jiges korli
-O kokhon toke msg koreche?
-Kire
-915 te
-Oh
-O kikore janlo?
-Moi ki kore janbo
-Acha bad "
+print('Sei din scale er kotha kno extra pakamo jiges korli O kokhon toke msg koreche? Kire 915 te Oh O kikore janlo? Moi ki kore janbo Acha bad')
